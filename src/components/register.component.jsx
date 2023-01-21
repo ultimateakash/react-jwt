dd6b1913b54f44469ab5b46bc14c85e4106d2ab2
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2023-01-21 21:19:54 +0530

    Add password pattern validation

--- src/components/register.component.jsx
+++ src/components/register.component.jsx
@@ -5,23 +5,27 @@ import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import * as Yup from 'yup';
 import authService from '../services/auth.service';
+import { useState } from 'react';
 
 const RegisterComponent = () => {
 
   const navigate = useNavigate();
+  const [isSubmitted, setIsSubmitted] = useState();
 
   const schema = Yup.object().shape({
     name: Yup.string().required(),
     email: Yup.string().email().required(),
     password: Yup.string().required()
+      .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/, 'Password should contains a lowercase, a uppercase character and a digit.')
   });
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const { register, handleSubmit, formState: { errors, isDirty, isValid } } = useForm({
     mode: 'all',
     resolver: yupResolver(schema)
   });
 
   const handleValidSubmit = async (data) => {
+    setIsSubmitted(true)
     try {
       const result = await authService.register(data);
       if (result.data) {
@@ -30,6 +34,7 @@ const RegisterComponent = () => {
     } catch (error) {
       toast.error(error.data.message);
     }
+    setIsSubmitted(false)
   }
 
   return (
@@ -57,7 +62,7 @@ const RegisterComponent = () => {
               {errors.password && <p>{errors.password.message}</p>}
             </div>
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary" disabled={isSubmitted || !isDirty || !isValid}>Submit</button>
         </form>
       </div>
     </div>
